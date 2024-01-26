27248742d6f259f5959eec42439ac993819b5d1d
Crissancio <soyelpapu1509@gmail.com>
2024-01-26 09:06:30 -0400

    commit_1_26-01-24

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                hostname: 'https://raw.githubusercontent.com/',
+            }
+        ],
+    }
+};
+
+//75    
 
 export default nextConfig;
---
+++ src/app/components/pokedex.js
@@ -0,0 +1,11 @@
+'use client'
+
+import style from './pokedex.module.css';
+
+export default function Pokedex(){
+    return(
+        <div className={style.appContainer}>
+            
+        </div>
+    );
+}
